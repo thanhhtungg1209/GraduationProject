62b7f84d2ed2169ad9b47293589f139fdc838dc7
tahuuducknowleadge <168318972+tahuuducknowleadge@users.noreply.github.com>
2024-12-06 17:59:54 +0700

    RegisterTest

---
+++ src/main/java/pages/RegisterPage.java
@@ -0,0 +1,91 @@
+package pages;
+
+import config.ConfigManager;
+import core.Config;
+import core.Utils;
+import org.openqa.selenium.*;
+
+public class RegisterPage {
+    public static Config[] webConfigs = ConfigManager.readTestObjectsFromJson("src/main/resources/config.json");
+    public static Config[] registerConfigs = ConfigManager.readTestObjectsFromJson("src/main/resources/register.json");
+    private static final String emailXpath = "//input[@name='email']";
+    private static final String firstNameXpath = "//input[@name='first-name']";
+    private static final String lastNameXpath = "//input[@name='last-name']";
+    private static final String passwordXpath = "//input[@name='password']";
+    private static final String createButtonXpath = "//div[contains(text(),'Create an account')]";
+
+    private static WebDriver driver;
+    public RegisterPage(WebDriver driver){
+        RegisterPage.driver = driver;
+    }
+
+    /**
+     * Điều hướng đến trang chủ
+     */
+    public static void goToPage() {
+        driver.get(webConfigs[0].getBaseUrl());
+        Utils.sleep(2);
+    }
+
+    /**
+     * Đăng ký tài khoản
+     */
+    public static void register(int i) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();",driver.findElement(By.xpath("//button[@aria-label='Account']//span[@aria-label='Account']")));
+        Utils.sleep(2);
+        Utils.waitForElementClickable(By.xpath("//button[normalize-space()='Register today']"), 10);
+        driver.findElement(By.xpath("//button[normalize-space()='Register today']")).click();
+        Utils.waitForElementVisible(By.xpath(emailXpath), 10);
+        driver.findElement(By.xpath(emailXpath)).sendKeys(registerConfigs[i].getEmail());
+        Utils.waitForElementClickable(By.xpath(firstNameXpath), 10);
+        driver.findElement(By.xpath(firstNameXpath)).sendKeys(registerConfigs[i].getFirstName());
+        Utils.waitForElementClickable(By.xpath(lastNameXpath), 10);
+        driver.findElement(By.xpath(lastNameXpath)).sendKeys(registerConfigs[i].getLastName());
+        Utils.waitForElementVisible(By.xpath(passwordXpath), 10);
+        driver.findElement(By.xpath(passwordXpath)).sendKeys(registerConfigs[i].getPassword());
+        driver.findElement(By.xpath("//span[contains(text(),'Sign Up for Newsletter')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'I want to create an account')]")).click();
+        Utils.waitForElementVisible(By.xpath(createButtonXpath), 10);
+        driver.findElement(By.xpath(createButtonXpath)).click();
+    }
+
+    /**
+     * Check đăng ký không thành công
+     * @return
+     */
+    public static boolean checkRegisterFail(){
+        try {
+            Utils.verifyElementDisplay("//span[contains(text(),'A customer with the same email address already exists in an associated website.')]");
+            return true;
+        }catch (NoSuchElementException e){
+            return false;
+        }
+    }
+
+    /**
+     * Check email invalid
+     * @return
+     */
+    public static boolean checkEmailInvalid(){
+        try{
+            Utils.verifyElementDisplay("//div[contains(text(),'The email field must be a valid email')]");
+            return true;
+        }catch (NoSuchElementException e){
+            return false;
+        }
+    }
+
+    /**
+     * Check password invalid
+     * @return
+     */
+    public static boolean checkPasswordInvalid(){
+        try {
+            Utils.verifyElementDisplay("//div[contains(text(),'The password must be at least 8 characters long and must contain at least: 1 uppercase or lowercase letter, 1 number, or one special character (E.g. , . _ & ? etc)')]");
+            return true;
+        }catch (NoSuchElementException e){
+            return false;
+        }
+    }
+}
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,5 +1,49 @@
 import core.BaseTest;
+import core.Utils;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.ProfilePage;
+import pages.RegisterPage;
 
 public class RegisterTest extends BaseTest {
+    @BeforeClass
+    public void Init(){
+        RegisterPage registerPage = new RegisterPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        ProfilePage profilePage = new ProfilePage(driver);
+        Utils utils = new Utils(driver);
+        RegisterPage.goToPage();
+    }
 
+    @Test(priority = 1, description = "Đảm bảo người dùng có thể đăng ký tài khoản thành công khi cung cấp thông tin hợp lệ.")
+    public void DK_1() {
+        int i=0;
+        RegisterPage.register(i);
+        LoginPage.login(i);
+        ProfilePage.goToProfile();
+        Assert.assertTrue(LoginPage.checkLoginSuccess());
+    }
+
+    @Test(priority = 2, description = "Đảm bảo rằng hệ thống không cho phép người dùng đăng ký với địa chỉ email đã tồn tại.")
+    public void DK_2(){
+        int i=0;
+        RegisterPage.register(i);
+        Assert.assertTrue(RegisterPage.checkRegisterFail());
+    }
+
+    @Test(priority = 3, description = "Đảm bảo rằng hệ thống không cho phép đăng ký với email không hợp lệ.")
+    public void DK_3(){
+        int i=1;
+        RegisterPage.register(i);
+        Assert.assertTrue(RegisterPage.checkEmailInvalid());
+    }
+
+    @Test(priority = 4, description = "Đảm bảo rằng hệ thống không cho phép đăng ký với mật khẩu quá ngắn.")
+    public void DK_4(){
+        int i=2;
+        RegisterPage.register(i);
+        Assert.assertTrue(RegisterPage.checkPasswordInvalid());
+    }
 }
