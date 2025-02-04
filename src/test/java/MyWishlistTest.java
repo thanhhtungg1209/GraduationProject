84e37acad6369211013cc2550bc0e7a481caf5ed
ducth <tahuuduc.knowledge@gmail.com>
2025-02-04 13:56:55 +0700

    04/02/2025

--- src/test/java/MyWishlistTest.java
+++ src/test/java/MyWishlistTest.java
@@ -1,5 +1,26 @@
 import core.BaseTest;
+import core.Utils;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.MyWishlistPage;
 
 public class MyWishlistTest extends BaseTest {
-    
+    private static int methodCount;
+    @BeforeClass
+    public void Init(){
+        MyWishlistPage myWishlistPage = new MyWishlistPage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        Utils utils = new Utils(driver);
+    }
+
+    @Test(priority = 1,description = "Kiểm tra chức năng thêm sản phẩm vào danh sách yêu thích")
+    public void MW_1(){
+        int i=0;
+        LoginPage.goToPage();
+        LoginPage.login(i);
+        MyWishlistPage.addMyWishlist(i);
+        MyWishlistPage.goToMyWishlistPage();
+        Utils.verifyElementDisplay("//span[contains(text(),'"+MyWishlistPage.myWishlistConfigs[i].getName()+"')]");
+    }
 }
